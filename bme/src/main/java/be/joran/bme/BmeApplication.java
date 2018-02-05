e408193b2c4394a3ac6ad5f075b3e438c88d0ff4
Joran Claessens <11400919@student.pxl.be>
2018-02-05 19:39:52 +0100

    Created Backend project

---
+++ bme/src/main/java/be/joran/bme/BmeApplication.java
@@ -0,0 +1,12 @@
+package be.joran.bme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmeApplication.class, args);
+	}
+}
